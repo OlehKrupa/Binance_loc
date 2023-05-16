566e6986e1f7bbb9c17942f53f53b404ca6fcadc
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-05-16 23:43:46 +0300

    Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -10,6 +12,8 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class;
+    
     /**
      * Define the model's default state.
      *
@@ -18,12 +22,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'email'
+            => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
-        ];
+            ];
     }
 
     /**
