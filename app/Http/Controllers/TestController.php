7dc08fdb1a22a844f94c915c7e5f7ad1d1a9967c
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-05-25 19:57:09 +0300

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,35 @@ class TestController extends Controller
 {
     public function index()
     {
-        phpinfo();
+        $url = 'https://api.coinbase.com/v2/prices/BTC-USD/buy';
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        $data = json_decode($response, true);
+        
+        // Дальнейшая обработка полученных данных
+        // ...
+        dump($data); 
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => 'https://api.example.com/cryptocurrencies', // Замените URL на соответствующий API для получения списка криптовалют
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+        ]);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $cryptocurrencies = json_decode($response, true); // Предполагается, что API возвращает JSON со списком криптовалют
+
+
+        dump($cryptocurrencies);
     }
    
 }
