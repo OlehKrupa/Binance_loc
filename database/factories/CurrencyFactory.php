bb595d6fbebc577d57fbf63ac632c2abfb9ae876
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-05-27 21:54:15 +0300

    Update CurrencyFactory.php

--- database/factories/CurrencyFactory.php
+++ database/factories/CurrencyFactory.php
@@ -19,13 +19,17 @@ class CurrencyFactory extends Factory
      */
     public function definition(): array
     {
-        $cryptocurrencies = [
-            'BTC', 'ETH', 'BNB', 'XRP', 'DOGE', 'ADA', 'LTC', 'DOT', 'BCH', 'LINK',
-            'XLM', 'USDT', 'VET', 'ETC', 'XMR', 'EOS', 'TRX', 'THETA', 'FIL', 'AAVE',
-            'XTZ', 'MIOTA', 'ATOM', 'CRO', 'MKR', 'ALGO', 'SOL', 'NEO', 'SNX', 'AVAX',
-            'UNI', 'COMP', 'SUSHI', 'YFI', 'CRV', 'REN', 'UMA', 'BAL', 'KSM', 'ICX',
-            'ZEC', 'DASH', 'OMG', 'GRT', 'SC', 'SRM', 'ENJ', 'QTUM', 'MANA', 'HOT',
-        ];
+        $cryptocurrencies = array(
+            "BTC", "ETH", "BNB", "ADA", "XRP",
+            "DOGE", "DOT", "BCH", "LTC", "LINK",
+            "XLM", "MATIC", "ETC", "THETA", "VET",
+            "FIL", "TRX", "XMR", "EOS", "NEO",
+            "KLAY", "ATOM", "IOTA", "WBTC", "LUNA",
+            "BSV", "AAVE", "XTZ", "FTT", "CRO",
+            "ALGO", "MKR", "COMP", "AVAX", "TFUEL",
+            "CAKE", "HT", "DAI", "BTT", "SUSHI",
+            "ZEC", "EGLD", "YFI", "UMA"
+        );
 
         $randomCurrency = $this->faker->unique()->randomElement($cryptocurrencies);
 
