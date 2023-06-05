5c8ca182c06db45bdedcaf1130f6a62bb4332aab
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-06-05 10:38:17 +0300

    Update CurrencyHistory.php
    Fix duplicates of currencies

--- app/Models/CurrencyHistory.php
+++ app/Models/CurrencyHistory.php
@@ -25,26 +25,32 @@ class CurrencyHistory extends Model
 
     public static function getLastCurrencies($selectedCurrencies)
     {
-        return self::select('currency.id', 'currency.name', 'currency_history.sell', 'currency_history.buy')
-            ->join('currency', 'currency_history.currency_id', '=', 'currency.id')
-            ->join('user_currency', 'currency.id', '=', 'user_currency.currency_id')
-            ->join('users', 'user_currency.user_id', '=', 'users.id')
-            ->whereIn('currency.id', $selectedCurrencies)
-            ->latest('currency_history.created_at')
-            ->take(count($selectedCurrencies))
-            ->get();
+        return self::select('currency.id', 'currency.name', 'currency_history.sell', 'currency_history.buy', 'currency_history.created_at')
+        ->join('currency', 'currency_history.currency_id', '=', 'currency.id')
+        ->join('user_currency', 'currency.id', '=', 'user_currency.currency_id')
+        ->join('users', 'user_currency.user_id', '=', 'users.id')
+        ->whereIn('currency.id', $selectedCurrencies)
+        ->latest('currency_history.created_at')
+        ->distinct()
+        ->take(count($selectedCurrencies))
+        ->get();
+        
     }
 
     public static function getDayCurrencies($selectedCurrencies)
     {
-        $startDate = Carbon::now();
+        $startDate = Carbon::now()->startOfDay();
+        $endDate = Carbon::now()->endOfDay();
 
         return self::select('currency.id', 'currency.name', 'currency_history.sell', 'currency_history.buy', 'currency_history.updated_at')
-            ->join('currency', 'currency_history.currency_id', '=', 'currency.id')
-            ->join('user_currency', 'currency.id', '=', 'user_currency.currency_id')
-            ->join('users', 'user_currency.user_id', '=', 'users.id')
-            ->whereIn('currency.id', $selectedCurrencies)
-            ->whereDate('currency_history.created_at', $startDate)
-            ->get();
+        ->join('currency', 'currency_history.currency_id', '=', 'currency.id')
+        ->join('user_currency', 'currency.id', '=', 'user_currency.currency_id')
+        ->join('users', 'user_currency.user_id', '=', 'users.id')
+        ->whereIn('currency.id', $selectedCurrencies)
+        ->whereBetween('currency_history.created_at', [$startDate, $endDate])
+        ->distinct()
+        ->get();
+
     }
+
 }
