15936297322128391f5fad956953626a7709055a
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-06-05 22:10:16 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller
 
         // Get last currencies
         $currenciesHistory = CurrencyHistory::getLastCurrencies($selectedCurrencies);
-
+        
         return view('home')->with('currenciesHistory', $currenciesHistory);
     }
 }
