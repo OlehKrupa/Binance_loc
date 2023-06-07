1695dd5013e1b211f77ad33856ada62214527dec
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-06-07 20:51:58 +0300

    Create PreferencesUpdateRequest.php

---
+++ app/Http/Requests/PreferencesUpdateRequest.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PreferencesUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; // Установите в true, если авторизация не требуется для этого запроса
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $maxCryptocurrencies = config('services.max_cryptocurrencies');
+
+        return [
+            'selectedCurrencies' => [
+                'required',
+                'array',
+                'min:1',
+                'max:' . $maxCryptocurrencies,
+            ],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        $maxCryptocurrencies = config('services.max_cryptocurrencies');
+
+        return [
+            'selectedCurrencies.required' => 'Please select at least one cryptocurrency.',
+            'selectedCurrencies.array' => 'The selected currencies must be an array.',
+            'selectedCurrencies.min' => 'Please select at least one cryptocurrency.',
+            'selectedCurrencies.max' => 'You can select a maximum of ' . $maxCryptocurrencies . ' cryptocurrencies.',
+        ];
+    }
+}
