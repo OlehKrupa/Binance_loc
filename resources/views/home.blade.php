ae020e88682d0f374713aecdbf718ec02fceaf2e
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-06-12 22:40:37 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
-
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Yours currencies') }}</div>
+                <div class="card-header">{{ __('Your Currencies') }}</div>
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
-
                     <!-- Add Chart.js graph -->
                     <div>
                         <canvas id="myChart"></canvas>
                     </div>
-
                     <!-- Add DataTable -->
                     <table id="currencyTable" class="table">
                         <thead>
@@ -29,7 +27,7 @@
                         </thead>
                         <tbody>
                             @foreach ($currenciesHistory as $currency)
-                            <tr>
+                            <tr onclick="getID({{ $currency->id }})">
                                 <td>{{ $currency->name }}</td>
                                 <td>${{ number_format($currency->buy, 2) }}</td>
                                 <td>${{ number_format($currency->sell, 2) }}</td>
@@ -45,42 +43,76 @@
 @endsection
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
 <script>
-    // Chart.js
+// Chart.js
     $(document).ready(function() {
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'line', // Line chart to display Bitcoin price
-            data: {
-                labels: ['12:00 AM', '3:00 AM', '6:00 AM', '9:00 AM', '12:00 PM', '3:00 PM', '6:00 PM', '9:00 PM'], // Time intervals
-                datasets: [{
-                    label: 'Bitcoin Price',
-                    data: [45000, 45200, 45500, 45300, 45800, 46000, 45800, 45500], // Bitcoin price data for the day
-                    backgroundColor: 'rgba(0, 123, 255, 0.5)',
-                    borderColor: 'rgba(0, 123, 255, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        ticks: {
-                            callback: function(value, index, values) {
-                                return '$' + value; // Display dollar sign with y-axis values
-                            }
-                        }
-                    }
-                }
-            }
-        });
+        var selectedCurrencyId = {!! $currenciesHistory->first()->id !!};
+
+        window.getID = function(id) {
+            selectedCurrencyId = id;
+            updateChart(selectedCurrencyId);
+        }
+
+        function updateChart(selectedCurrencyId) {
+            console.log(selectedCurrencyId);
+
+            var labels = {!! $dayCurrencies->where('id', 171)->pluck('updated_at') !!};
+            var data = {!! $dayCurrencies->where('id', 171)->pluck('sell') !!};
+
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels.map(function(label) {
+                        var date = new Date(label);
+                        return formatDate(date);
+                    }),
+                    datasets: [{
+                        label: 'Currency Price',
+                        data: data,
+                        backgroundColor: 'rgba(0, 123, 255, 0.5)',
+                        borderColor: 'rgba(0, 123, 255, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    legend: {
+                        display: true,
+                        position: 'top',
+                        labels: {
+                          boxWidth: 80,
+                          fontColor: 'black'
+                      }
+                  }
+              }
+          });
+        }
 
     // DataTable
         $('#currencyTable').DataTable({
+            paging: false,
+            searching: false,
+            language: {
+              info: "Select a cryptocurrency to display the chart"
+          }
+      });
 
-        });
+
+    // Initial chart update
+        updateChart(selectedCurrencyId);
     });
+
+    function formatDate(date) {
+        var options = {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+        };
+        return date.toLocaleDateString('en-GB', options).replace(',', '');
+    }
 </script>
