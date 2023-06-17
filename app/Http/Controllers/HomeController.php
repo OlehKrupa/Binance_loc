58c4260e351e8a1b564a97f4765ac06ad97079f8
Oleh <95093144+OlehKrupa@users.noreply.github.com>
2023-06-17 21:22:03 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,17 +6,27 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\CurrencyHistory;
 use Carbon\Carbon;
+use Illuminate\Session\Store;
 
 class HomeController extends Controller
 {
     /**
+     * The session store instance.
+     *
+     * @var \Illuminate\Session\Store
+     */
+    protected $session;
+
+    /**
      * Create a new controller instance.
      *
+     * @param  \Illuminate\Session\Store  $session
      * @return void
      */
-    public function __construct()
+    public function __construct(Store $session)
     {
         $this->middleware('auth');
+        $this->session = $session;
     }
 
     /**
@@ -29,24 +39,33 @@ class HomeController extends Controller
         // Get current user
         $user = auth()->user();
 
-        // Get start day
-        //сделать запоминание выбранного периода
-        $startDate = $request->input('dateRange', 1);
+        // Get start day from session or use default value (1)
+        $startDate = $this->session->get('startDate', 1);
+        // Update start day if provided in the request
+        if ($request->has('dateRange')) {
+            $startDate = $request->input('dateRange');
+            // Save the updated start day to session
+            $this->session->put('startDate', $startDate);
+        }
 
         // Get selected user currencies 
         $selectedCurrencies = $user->currencies()->pluck('currency_id');
 
-        // Get choosen currency
-        //сделать запоминание выбранной крипты
-        $choosenID = $request->input('currencyId', $selectedCurrencies->first());
+        // Get chosen currency from session or use default value (first currency)
+        $choosenID = $this->session->get('choosenID', $selectedCurrencies->first());
+        // Update chosen currency if provided in the request
+        if ($request->has('currencyId')) {
+            $choosenID = $request->input('currencyId');
+            // Save the updated chosen currency to session
+            $this->session->put('choosenID', $choosenID);
+        }
 
         // Get day currencies
         $dayCurrencies = CurrencyHistory::getDayCurrencies($selectedCurrencies, $startDate);
 
         return view('home')
-        ->with('dayCurrencies', $dayCurrencies) 
-        ->with('startDate',$startDate)
-        ->with('choosenID',$choosenID);
+            ->with('dayCurrencies', $dayCurrencies) 
+            ->with('startDate', $startDate)
+            ->with('choosenID', $choosenID);
     }
-
 }
