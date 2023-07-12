25a9a278987be3a99d5bb7a7aa28c92f9ab06d60
Oleh Krupa <krupao980@gmail.com>
2023-07-12 10:03:39 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->timestamp('subscribed_at');
+            $table->timestamp('subscribed_at')->nullable();
             $table->timestamps();
             $table->softDeletes();
             $table->rememberToken();
