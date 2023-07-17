823bce5cf5691c0e4784d52745ad1b03a813b9cf
Oleh Krupa <krupao980@gmail.com>
2023-07-17 13:18:55 +0300

    Update preferences.js

--- public/js/preferences.js
+++ public/js/preferences.js
@@ -4,13 +4,5 @@ var __webpack_exports__ = {};
   !*** ./resources/js/preferences.js ***!
   \*************************************/
 // Preferences DataTable
-$(document).ready(function () {
-  $('#currencyTable').DataTable({
-    scrollY: '600px',
-    scrollCollapse: true,
-    paging: false,
-    dom: 'f t'
-  });
-});
 /******/ })()
 ;
