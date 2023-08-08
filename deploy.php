a274086c955b4a006624cc13e09d03585dacee98
Oleh Krupa <krupao980@gmail.com>
2023-08-08 14:20:35 +0300

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -5,7 +5,8 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'https://github.com/OlehKrupa/Binance.loc.git');
+set('repository', 'git@github.com:OlehKrupa/Binance.loc.git'); 
+set('branch', 'Release');
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -13,11 +14,39 @@ add('writable_dirs', []);
 
 // Hosts
 
-//Доступ до дроплета == SSH tunnel
-host('example.org')
-    ->set('remote_user', 'laravel')
-    ->set('deploy_path', '~/example');
+host('164.90.162.49') // Droplet IP
+    ->user('remote_user','root') 
+    ->set('deploy_path', '/var/www/binanceapi'); 
+
+// Tasks
+
+task('deploy:composer', function () {
+    run('cd {{release_path}} && composer install --no-interaction --no-dev --prefer-dist');
+});
+
+task('deploy:artisan:migrate', function () {
+    run('{{bin/php}} {{release_path}}/artisan migrate --force');
+});
+
+//Currency fill
 
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
+
+// Deployment
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:composer',
+    'deploy:artisan:migrate',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
