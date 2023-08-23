d0fef86659282eefe36ea7cfee84c2e1a7522299
Oleh <krupao980@gmail.com>
2023-08-23 13:20:49 +0300

    typo

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\Api\StripeController;
 
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
-Route::get('/news', [NewsController::class,'getNews'])->name('news');
+Route::get('/news', [NewsController::class, 'getNews'])->name('news');
 
 Route::post('/webhook', [StripeController::class, 'webhook']);
 
@@ -34,14 +34,15 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::get('/user/getSession', [StripeController::class, 'getSession']);
+    Route::get('/user/unstripe', [StripeController::class, 'cancelSubscribe'])->name('auth.cancelSubscribe');
 
     Route::get('/user/preferencesData', [UserController::class, 'getPreferencesData'])->name('auth.getPreferencesData');
     Route::get('/user/preferences', [UserController::class, 'getPreferences'])->name('auth.getPreferences');
     Route::put('/user/preferences', [UserController::class, 'setPreferences'])->name('auth.setPreferences');
 
     Route::get('/user/history', [UserController::class, 'getUserCurrencyHistory'])->name('auth.getUserCurrencyHistory');
-    Route::patch('/user/subscribe', [UserController::class,'toggleSubscriptionStatus'])->name('auth.subscribe');
-    Route::patch('/user/unpremium', [UserController::class,'unTogglePremiumStatus'])->name('auth.unpremium');
+    Route::patch('/user/subscribe', [UserController::class, 'toggleSubscriptionStatus'])->name('auth.subscribe');
+    Route::patch('/user/unpremium', [UserController::class, 'unTogglePremiumStatus'])->name('auth.unpremium');
 
     Route::apiResource('/currency', CurrencyController::class);
     Route::apiResource('/history', HistoryController::class);
