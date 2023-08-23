2487194686f75c46284d1817bb6d1ea262dd84bf
Oleh <krupao980@gmail.com>
2023-08-23 15:01:41 +0300

    Update StripeController.php

--- app/Http/Controllers/Api/StripeController.php
+++ app/Http/Controllers/Api/StripeController.php
@@ -14,25 +14,6 @@ class StripeController extends Controller
 
         $user = auth()->user();
 
-        // $checkout = $stripe->checkout->sessions->create([
-        //     'success_url' => 'http://localhost:8080/preferences',
-        //     'cancel_url' => 'http://localhost:8080/dashboard',
-        //     'line_items' => [
-        //         [
-        //             'price_data' => [
-        //                 'currency' => 'usd',
-        //                 'unit_amount' => 500,
-        //                 'product_data' => [
-        //                     'name' => "Stripe controller Test",
-        //                 ],
-        //             ],
-        //             'quantity' => 1,
-        //         ],
-        //     ],
-        //     'customer_email' => $user->email,
-        //     'mode' => 'payment',
-        // ]);
-
         $sub = $stripe->checkout->sessions->create([
             'success_url' => 'http://localhost:8080/preferences',
             'cancel_url' => 'http://localhost:8080/dashboard',
@@ -42,53 +23,46 @@ class StripeController extends Controller
                     'quantity' => 1,
                 ],
             ],
-            'customer_email' => $user->email, 
+            'customer_email' => $user->email,
             'mode' => 'subscription',
         ]);
 
         return [
-            // 'oneTime' => $checkout, 
             'sub' => $sub
         ];
     }
 
-    public function cancelSubscribe(Request $request)
-{
-    $stripe = new \Stripe\StripeClient('sk_test_51Ng6pYDtedehY5ENIwiy6IZryUlzSwu4UMuQBXXNp1LVK6qJolEXUT16xQFaogy1wQpwpaKAcGTO0w06BLK2L9g100DGsTAl71');
+    public function cancelSubscribe()
+    {
+        $stripe = new \Stripe\StripeClient('sk_test_51Ng6pYDtedehY5ENIwiy6IZryUlzSwu4UMuQBXXNp1LVK6qJolEXUT16xQFaogy1wQpwpaKAcGTO0w06BLK2L9g100DGsTAl71');
 
-    $user = auth()->user();
+        $user = auth()->user();
 
-    $stripeCustomer = $stripe->customers->all([
-        'email' => $user->email,
-        'limit' => 1,
-    ]);
-
-    if (!empty($stripeCustomer->data)) {
-        $customerId = $stripeCustomer->data[0]->id;
-
-        $subscriptions = $stripe->subscriptions->all([
-            'customer' => $customerId,
-            'price' => 'price_1NhTtUDtedehY5EN7rLqIEr4',
+        $stripeCustomer = $stripe->customers->all([
+            'email' => $user->email,
+            'limit' => 1,
         ]);
 
-        if (!empty($subscriptions->data)) {
-            $subscriptionId = $subscriptions->data[0]->id;
-            $canceledSubscription = $stripe->subscriptions->update($subscriptionId, [
-                'cancel_at_period_end' => true,
+        if (!empty($stripeCustomer->data)) {
+            $customerId = $stripeCustomer->data[0]->id;
+
+            $subscriptions = $stripe->subscriptions->all([
+                'customer' => $customerId,
+                'price' => 'price_1NhTtUDtedehY5EN7rLqIEr4',
             ]);
 
-            // if ($canceledSubscription) {
-            //     $user->premium_at = null;
-            //     $user->save();
-            //     \Illuminate\Support\Facades\Log::info("Canceled premium subscription for user {$user->id}.");
-            // }
+            if (!empty($subscriptions->data)) {
+                $subscriptionId = $subscriptions->data[0]->id;
+                $stripe->subscriptions->update($subscriptionId, [
+                    'cancel_at_period_end' => true,
+                ]);
 
-            return response()->json(["status" => 'success']);
+                return response()->json(["status" => 'success']);
+            }
         }
-    }
 
-    return response()->json(["status" => 'error', "message" => 'No active subscriptions found.']);
-}
+        return response()->json(["status" => 'error', "message" => 'No active subscriptions found.']);
+    }
 
 
     public function webhook(Request $request)
